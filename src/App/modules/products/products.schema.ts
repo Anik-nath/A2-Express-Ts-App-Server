2063ccb9766207368879c6e36941e1a114e7a90d
Anik-Nath <anikdevnath03112000@gmail.com>
2024-05-21 11:11:37 +0600

    handle auto generate versionkey and _id

--- src/App/modules/products/products.schema.ts
+++ src/App/modules/products/products.schema.ts
@@ -3,23 +3,32 @@ import { Schema } from 'mongoose';
 import { Inventory, Iproduct, Variant } from './products.interface';
 
 // variant schema
-const variantSchema = new Schema<Variant>({
-  type: { type: String, required: true },
-  value: { type: String, required: true },
-});
+const variantSchema = new Schema<Variant>(
+  {
+    type: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+  { _id: false }
+);
 // inventory schema
-const inventorySchema = new Schema<Inventory>({
-  quantity: { type: Number, required: true },
-  inStock: { type: Boolean, required: true },
-});
+const inventorySchema = new Schema<Inventory>(
+  {
+    quantity: { type: Number, required: true },
+    inStock: { type: Boolean, required: true },
+  },
+  { _id: false }
+);
 //product schema
-const productSchema = new Schema<Iproduct>({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  category: { type: String, required: true },
-  tags: { type: [String], required: true },
-  variants: { type: [variantSchema], required: true },
-  inventory: { type: inventorySchema, required: true },
-});
+const productSchema = new Schema<Iproduct>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    tags: { type: [String], required: true },
+    variants: { type: [variantSchema], required: true },
+    inventory: { type: inventorySchema, required: true },
+  },
+  { versionKey: false },
+);
 export default productSchema;

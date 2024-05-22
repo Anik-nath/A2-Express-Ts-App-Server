67c7d3a54d9d8faabf132bd1638ffcb4f64202cf
Anik-Nath <anikdevnath03112000@gmail.com>
2024-05-22 23:13:07 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { productRoutes } from './App/modules/products/procducts.route';
 import { orderRoutes } from './App/modules/orders/orders.route';
-// import { errorhandler } from './App/modules/errorHandler/error.handler';
 
 const app = express();
 
@@ -13,12 +12,6 @@ app.use(cors());
 // api endpoints
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
-// app.all('*', (req: Request, res: Response) => {
-//   res.status(400).json({
-//     success: false,
-//     message: 'Route not found',
-//   });
-// });
 
 app.get('/', (req: Request, res: Response) => {
   const assignmentNo: number = 2;
